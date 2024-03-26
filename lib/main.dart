72df99d39942f4b9e75b78382b4b78c19cdc670b
Padmanabhuni Venkata Saiteja <saitejapadmanabhunivenkata@PadmanahunisMBP.Dlink>
2024-03-26 18:18:04 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:calculator/splash_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashPage(),
+    ),
+  );
+}
